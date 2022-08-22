eb66739f58ee1ee95f694fa94ceff78067112a00
Administrator <2762383426@qq.com>
2022-08-22 11:52:36 +0800

    修复代理问题

--- test/__init__.py
+++ test/__init__.py
@@ -1,39 +1,10 @@
-##最小二乘法
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import leastsq
+from binance.spot import Spot as Client
 
-plt.rcParams['font.sans-serif'] = ['SimHei']
+proxies = {'https': 'http://127.0.0.1:7890'}
 
-Xi = np.array([1, 2, 3])
-Yi = np.array([1, 3, 3])
-
-
-def func(p, x):
-    k, b = p
-    return k * x + b
-
-
-def error(p, x, y):
-    return func(p, x) - y
-
-
-p0 = [1, 1]
-
-Para = leastsq(error, p0, args=(Xi, Yi))
-
-k, b = Para[0]
-print("k=", k, "b=", b)
-print("cost：" + str(Para[1]))
-print("求解的拟合直线为:")
-print("y=" + str(round(k, 2)) + "x+" + str(round(b, 2)))
-
-plt.figure(figsize=(8, 6))
-plt.scatter(Xi, Yi, color="green", label="样本数据", linewidth=2)
-
-x = np.array([1, 2, 3])
-y = k * x + b
-plt.plot(x, y, color="red", label="拟合直线", linewidth=2)
-plt.title('y={}+{}x'.format(b, k))
-plt.legend(loc='lower right')
-plt.show()
+client = Client(proxies=proxies)
+print(client.time())
+# Get klines of BTCUSDT at 1m interval
+print(client.klines("BTCUSDT", "1m"))
+# Get last 10 klines of BNBUSDT at 1h interval
+print(client.klines("BNBUSDT", "1h", limit=10))
